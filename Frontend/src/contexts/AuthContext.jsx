2b604f1da5fd8780e492c2cf6b10d1e546865352
Prashant Kumar Tongad <prashant.14219011622@ipu.ac.in>
2026-01-24 16:40:31 +0530

    Update AuthContext.jsx

--- Frontend/src/contexts/AuthContext.jsx
+++ Frontend/src/contexts/AuthContext.jsx
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import axios from 'axios';
-import {API} from "../src/config";
+import {API} from "../config";
 const AuthContext = createContext();
 
 export const useAuth = () => useContext(AuthContext);
